86775408ccc0534fff3aa7c5d316d512b8921d49
binikiya <biniyamkiya3682@gmail.com>
2025-08-20 20:10:43 +0300

    Event and users model updated with requird fields

--- Event_Management_API/Event_Management_API/settings.py
+++ Event_Management_API/Event_Management_API/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    
+    'users',
+    'events',
+    'registrations',
+    'notifications',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#AUTH_USER_MODEL = 'users.User'
--- Event_Management_API/events/models.py
+++ Event_Management_API/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    date_time = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+    is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organized_events")
+
+    def __str__(self):
+        return self.title
