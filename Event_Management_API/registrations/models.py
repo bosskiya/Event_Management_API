3e9c4bc68b3c996ba225c4ddca7f21d36f92366e
binikiya <biniyamkiya3682@gmail.com>
2025-08-20 20:11:34 +0300

    Registration model updated with requird fields

--- Event_Management_API/registrations/models.py
+++ Event_Management_API/registrations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+from events.models import Event
 
-# Create your models here.
+class Registration(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='registrations')
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='registrations')
+    registered_at = models.DateTimeField(auto_now_add=True)
+    is_waitlisted = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ['user', 'event']
